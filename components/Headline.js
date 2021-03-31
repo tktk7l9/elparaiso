07a97090d53c28e254538e25764dc7b7aea0bada
tktk7l9 <saitotakuya0719@gmail.com>
2021-03-31 23:42:07 +0900

    ✨ Melodies, Projects, Store, Libraryページを追加

--- components/Headline.js
+++ components/Headline.js
@@ -1,14 +1,20 @@
+import Link from 'next/link'
 import classes from './Headline.module.css'
 
 export function Headline(props) {
     return (
         <div>
             <h1 className={classes.title}>
-                {props.page} page
+                {props.page}
             </h1>
             <p className={classes.description}>
-                Get started by editing {props.children}
+                {props.children}
             </p>
+            <Link href="/">Index </Link>
+            <Link href="/about">About </Link>
+            <Link href="/melodies">Melodies </Link>
+            <Link href="/projects">Projects </Link>
+            <Link href="/store">Store</Link>
         </div>
     )
 }
---
+++ pages/store.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import classes from '../styles/Home.module.css'
+import { Footer } from '../components/Footer'
+import { Main } from '../components/Main'
+
+export default function Home() {
+  return (
+    <div className={classes.container}>
+      <Head>
+        <title>Store - EL PARAISO</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Main page="store" />
+      <Footer/>
+    </div>
+  )
+}
