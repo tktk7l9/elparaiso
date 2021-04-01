4bb91e67f60ca89cb713e74a0fc76f3bccae4689
tktk7l9 <saitotakuya0719@gmail.com>
2021-04-01 23:49:02 +0900

    ✨ IndexページにCardsコンポーネントを追加

---
+++ components/Cards.js
@@ -0,0 +1,41 @@
+import classes from './Cards.module.css'
+
+export function Cards() {
+  return (
+    <div className={classes.grid}>
+      <div className={classes.gridItemContainer}>
+        <img src="images/post24.jpg"/>
+      </div>
+      <div className={classes.gridItemContainer}>
+        <img src="images/post23.jpg"/>
+      </div>
+      <div className={classes.gridItemContainer}>
+        <img src="images/post22.jpg"/>
+      </div>
+      <div className={classes.gridItemContainer}>
+        <img src="images/post21.jpg"/>
+      </div>
+      <div className={classes.gridItemContainer}>
+        <img src="images/post19.jpg"/>
+      </div>
+      <div className={classes.gridItemContainer}>
+        <img src="images/post18.jpg"/>
+      </div>
+      <div className={classes.gridItemContainer}>
+        <img src="images/post17.jpg"/>
+      </div>
+      <div className={classes.gridItemContainer}>
+        <img src="images/post16.jpg"/>
+      </div>
+      <div className={classes.gridItemContainer}>
+        <img src="images/post15.jpg"/>
+      </div>
+      <div className={classes.gridItemContainer}>
+        <img src="images/post14.jpg"/>
+      </div>
+      <div className={classes.gridItemContainer}>
+        <img src="images/post13.jpg"/>
+      </div>
+    </div>
+  )
+}
--- components/Main.js
+++ components/Main.js
@@ -1,16 +1,28 @@
 import classes from './Main.module.css'
+import { Cards } from '../components/Cards'
 import { Links } from '../components/Links'
 import { Headline } from '../components/Headline'
 
 export function Main(props) {
   return (
-      <main className={classes.main}>
-        <Headline page={props.page}>
-          <code className={classes.code}>
-            {props.page}
-          </code>
-        </Headline>
-        <Links />
-      </main>
+    <div className={classes.container}>
+      { props.page === "index"
+        ?
+        <main className={classes.main}>
+          <Headline page={props.page}>
+          </Headline>
+          <Cards />
+        </main>
+        :
+        <main className={classes.main}>
+          <Headline page={props.page}>
+            <code className={classes.code}>
+              {props.page}
+            </code>
+          </Headline>
+          <Links />
+        </main>
+      }
+    </div>
   )
 }
