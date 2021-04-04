b6084a241cc80676e5203b96bf64fed34e078ade
tktk7l9 <saitotakuya0719@gmail.com>
2021-04-04 23:53:11 +0900

    💄 カードのレイアウト変更

--- components/Cards.js
+++ components/Cards.js
@@ -3,38 +3,32 @@ import classes from './Cards.module.css'
 export function Cards() {
   return (
     <div className={classes.grid}>
-      <div className={classes.gridItemContainer}>
+      <div className={classes.topLeft}>
         <img src="images/post24.jpg"/>
       </div>
-      <div className={classes.gridItemContainer}>
+      <div className={classes.topCenter}>
         <img src="images/post23.jpg"/>
       </div>
-      <div className={classes.gridItemContainer}>
+      <div className={classes.topRight}>
         <img src="images/post22.jpg"/>
       </div>
-      <div className={classes.gridItemContainer}>
+      <div className={classes.centerLeft}>
         <img src="images/post21.jpg"/>
       </div>
-      <div className={classes.gridItemContainer}>
+      <div className={classes.centerCenter}>
         <img src="images/post19.jpg"/>
       </div>
-      <div className={classes.gridItemContainer}>
+      <div className={classes.centerRight}>
         <img src="images/post18.jpg"/>
       </div>
-      <div className={classes.gridItemContainer}>
+      <div className={classes.bottomLeft}>
         <img src="images/post17.jpg"/>
       </div>
-      <div className={classes.gridItemContainer}>
+      <div className={classes.bottomCenter}>
         <img src="images/post16.jpg"/>
       </div>
-      <div className={classes.gridItemContainer}>
+      <div className={classes.bottomRight}>
         <img src="images/post15.jpg"/>
-      </div>
-      <div className={classes.gridItemContainer}>
-        <img src="images/post14.jpg"/>
-      </div>
-      <div className={classes.gridItemContainer}>
-        <img src="images/post13.jpg"/>
       </div>
     </div>
   )
