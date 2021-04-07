cdedb385bf2005a223daf4013d4e3a406a0993a3
tktk7l9 <saitotakuya0719@gmail.com>
2021-04-07 22:39:02 +0900

    💄 カードの構成変更

--- components/Cards.js
+++ components/Cards.js
@@ -1,34 +1,38 @@
+import Link from 'next/link'
 import classes from './Cards.module.css'
 
 export function Cards() {
   return (
     <div className={classes.grid}>
       <div className={classes.topLeft}>
-        <img src="images/post24.jpg"/>
+        <Link href="/about">
+          <img src="images/post24.jpg"/>
+        </Link>
       </div>
       <div className={classes.topCenter}>
-        <img src="images/post23.jpg"/>
+        <Link href="/melodies">
+          <img src="images/post23.jpg" />
+        </Link>
       </div>
       <div className={classes.topRight}>
-        <img src="images/post22.jpg"/>
-      </div>
-      <div className={classes.centerLeft}>
-        <img src="images/post21.jpg"/>
-      </div>
-      <div className={classes.centerCenter}>
-        <img src="images/post19.jpg"/>
-      </div>
-      <div className={classes.centerRight}>
-        <img src="images/post18.jpg"/>
+        <Link href="/projects">
+          <img src="images/post22.jpg" />
+        </Link>
       </div>
       <div className={classes.bottomLeft}>
-        <img src="images/post17.jpg"/>
+        <Link href="/store">
+          <img src="images/post21.jpg" />
+        </Link>
       </div>
       <div className={classes.bottomCenter}>
-        <img src="images/post16.jpg"/>
+        <Link href="/library">
+          <img src="images/post19.jpg" />
+        </Link>
       </div>
       <div className={classes.bottomRight}>
-        <img src="images/post15.jpg"/>
+        <Link href="/">
+          <img src="images/post18.jpg" />
+        </Link>
       </div>
     </div>
   )
