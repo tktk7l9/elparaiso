df9dd103c1df2b97d36ac8c529ddec745a2d0f5f
tktk7l9 <saitotakuya0719@gmail.com>
2021-04-08 18:21:50 +0900

    ♻️ TitleからHomeへのアクセス

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,6 @@ import classes from './Header.module.css'
 export function Header() {
   return (
     <header className={classes.header}>
-      <Link href="/">
-        <a className={classes.anchor}>Index</a>
-      </Link>
       <Link href="/about">
         <a className={classes.anchor}>About</a>
       </Link>
--- components/Headline.js
+++ components/Headline.js
@@ -1,10 +1,13 @@
+import Link from 'next/link'
 import classes from './Headline.module.css'
 
 export function Headline(props) {
     return (
         <div>
             <h1 className={classes.title}>
-                EL PARAISO
+                <Link href="/">
+                    EL PARAISO
+                </Link>
             </h1>
             <p className={classes.description}>
                 find your paradise inside of you
