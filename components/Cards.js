2d89df5322eb15adedde44f2745fab30e23c9f59
tktk7l9 <saitotakuya0719@gmail.com>
2021-04-12 22:09:06 +0900

    ♻️ top画像のフォルダ移動

--- components/Cards.js
+++ components/Cards.js
@@ -6,32 +6,32 @@ export function Cards() {
     <div className={classes.grid}>
       <div className={classes.topLeft}>
         <Link href="/about">
-          <img src="images/post24.jpg"/>
+          <img src="images/top/post24.jpg"/>
         </Link>
       </div>
       <div className={classes.topCenter}>
         <Link href="/melodies">
-          <img src="images/post23.jpg" />
+          <img src="images/top/post23.jpg" />
         </Link>
       </div>
       <div className={classes.topRight}>
         <Link href="/projects">
-          <img src="images/post22.jpg" />
+          <img src="images/top/post22.jpg" />
         </Link>
       </div>
       <div className={classes.bottomLeft}>
         <Link href="/store">
-          <img src="images/post21.jpg" />
+          <img src="images/top/post21.jpg" />
         </Link>
       </div>
       <div className={classes.bottomCenter}>
         <Link href="/library">
-          <img src="images/post19.jpg" />
+          <img src="images/top/post19.jpg" />
         </Link>
       </div>
       <div className={classes.bottomRight}>
         <Link href="/">
-          <img src="images/post18.jpg" />
+          <img src="images/top/post18.jpg" />
         </Link>
       </div>
     </div>
