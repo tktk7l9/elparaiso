ae8b4730bd35967ee66b9457bb3acf88ba1a5ac7
tktk7l9 <saitotakuya0719@gmail.com>
2021-04-18 23:18:28 +0900

    ✨ Footerの更新

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,39 @@
 import classes from 'src/components/Footer/Footer.module.css'
 
 export function Footer() {
+  const LINKS = [
+    {
+      href: "https://www.instagram.com/elparaisojp/",
+      title: "Instagram"
+    },
+    {
+      href: "https://elparaiso.stores.jp/",
+      title: "Store"
+    },
+    {
+      href: "https://open.spotify.com/playlist/1jnkrS9FUGTzZ6nIOuZ0xE?si=70f4ef6442fb48f2",
+      title: "Spotify"
+    }
+  ]
   return (
     <footer className={classes.footer}>
-      Copyright © 2021 by Takuya Saito
+      <div className={classes.items}>
+        Culture & Policy
+      </div>
+      {LINKS.map((link) => {
+        return (
+          <div className={classes.items}>
+            <a
+              href={link.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={classes.links}
+            >
+              {link.title}
+            </a>
+          </div>
+        )
+      })}
     </footer>
   )
 }
