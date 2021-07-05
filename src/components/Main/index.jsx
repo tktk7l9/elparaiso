41121e0b3239bca138c73644b8666796a3160c65
tktk7l9 <saitotakuya0719@gmail.com>
2021-07-05 13:00:35 +0900

    💄 表示アニメーションの追加

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,33 +1,34 @@
-import classes from 'src/components/Main/Main.module.css'
-import { Cards } from 'src/components/Cards'
-import { About } from 'src/components/About'
-import { Spotify } from 'src/components/Spotify'
-import { Headline } from 'src/components/Headline'
-import { StoreItems } from 'src/components/StoreItems'
-import { PlaylistDetail } from 'src/components/PlaylistDetail'
-import { Contact } from 'src/components/Contact'
+import classes from "src/components/Main/Main.module.css";
+import { Cards } from "src/components/Cards";
+import { About } from "src/components/About";
+import { Spotify } from "src/components/Spotify";
+import { Headline } from "src/components/Headline";
+import { StoreItems } from "src/components/StoreItems";
+import { PlaylistDetail } from "src/components/PlaylistDetail";
+import { Contact } from "src/components/Contact";
 
 export function Main(props) {
-  const page = props.page
+  const page = props.page;
   return (
     <div className={classes.container}>
       <main className={classes.main}>
         <Headline page={props.page} />
-          { page === 'index'
-            ? <Cards />
-            : page === 'about'
-            ? <About />
-            : page === 'melodies'
-            ? <Spotify />
-            : page === 'playlist'
-            ? <PlaylistDetail />
-            : page === 'store'
-            ? <StoreItems />
-            : page === 'contact'
-            ? <Contact />
-            : <div>{page} page is coming soon</div>
-          }
+        {page === "index" ? (
+          <Cards />
+        ) : page === "about" ? (
+          <About />
+        ) : page === "melodies" ? (
+          <Spotify />
+        ) : page === "playlist" ? (
+          <PlaylistDetail />
+        ) : page === "store" ? (
+          <StoreItems />
+        ) : page === "contact" ? (
+          <Contact />
+        ) : (
+          <div className={classes.comingsoon}>{page} page is coming soon</div>
+        )}
       </main>
     </div>
-  )
+  );
 }
